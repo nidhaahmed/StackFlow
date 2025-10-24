d85fe01b3800176e250fc4f8be1708adb1fa04f6
nidhaahmed <nidhaahmed13@gmail.com>
2025-10-24 22:27:05 +0530

    Day 1: Initial Backend Setup

---
+++ stackflow-backend/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import path from "path";
+
+// dotenv.config({ path: path.resolve("stackflow-backend/.env") });
+dotenv.config();
+console.log("DB_URI:", process.env.DB_URI); 
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("StackFlow backend running 🚀"));
+
+// MongoDB connection
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
